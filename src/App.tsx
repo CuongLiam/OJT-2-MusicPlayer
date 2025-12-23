6f6e94376a3f8f126762d259c1291ad20ee5a91f
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-12-23 07:55:18 +0700

    signup-signin import links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import SignInModal from './components/auth/SignInModal'
+import SignUpModal from './components/auth/SignUpModal'
 
 export default function App() {
   return (
     <div>
       {/* <Header /> */}
-      <Footer />
+      {/* <Footer /> */}
+
+      {/* <SignInModal/> */}
+      <SignUpModal/>
     </div>
   )
 }
