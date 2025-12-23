0f77ffa72b150e112fea4e24778bdab54b4b8823
VirusBetaTester <breakgamer2601@gmail.com>
2025-12-23 23:02:50 +0700

    Develop-Artists and Fix-HeaderAndNavbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from './components/Header/Header'
 import Navbar from './components/Header/Navbar'
+import Artists from './components/Artist/Artists'
 import Footer from './components/Footer/Footer'
+// import SignInModal from './components/auth/SignInModal'
+// import SignUpModal from './components/auth/SignUpModal'
 
 export default function App() {
   return (
-    <div className="bg-[#1e2336] relative max-w-360 min-h-screen flex">
+    <div className="bg-[#0f1218] relative max-w-360 min-h-screen flex select-none">
       
       <Navbar />
       <div className="pl-20 w-full min-h-screen flex flex-col transition-all duration-300">
@@ -13,6 +16,7 @@ export default function App() {
 
         <main className="flex-1 p-6 text-white">
            <div className="h-full">
+            <Artists />
            </div>
         </main>
 
