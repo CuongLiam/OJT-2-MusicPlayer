9136617b3129d861edc1ae9735112856f1fe68ef
VirusBetaTester <breakgamer2601@gmail.com>
2025-12-23 08:11:25 +0700

    Resolve-Conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from './components/Header/Header'
 import Navbar from './components/Header/Navbar'
+import SignUpModal from './components/auth/SignUpModal'
 // import SignInModal from './components/auth/SignInModal'
-// import SignUpModal from './components/auth/SignUpModal'
-
 export default function App() {
   return (
     <div className="min-h-screen bg-[#1e2336] relative">
       <Navbar />
       <div className="pl-20 w-full transition-all duration-300">
-        <Header />       
+        <Header />
       </div>
+
+      {/* Modal */}
+      <SignUpModal />
     </div>
   )
 }
